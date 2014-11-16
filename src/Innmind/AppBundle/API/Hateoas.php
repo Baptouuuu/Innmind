84b4de15b0f39c152e3d9c75bf194452fc1d7e06
Baptiste Langlade <baptouuuu@gmail.com>
2014-11-16 13:32:23 +0100

    added a service to add links to api resource

---
+++ src/Innmind/AppBundle/API/Hateoas.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Innmind\AppBundle\API;
+
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Everyman\Neo4j\Relationship;
+
+/**
+ * Adds links to related resources
+ */
+
+class Hateoas
+{
+    protected $generator;
+
+    /**
+     * Set the url generator
+     *
+     * @param UrlGeneratorInterface $generator
+     */
+
+    public function setGenerator(UrlGeneratorInterface $generator)
+    {
+        $this->generator = $generator;
+    }
+
+    /**
+     * Add resource links for a node
+     *
+     * @param array $node
+     *
+     * @return array
+     */
+
+    public function handleNode(array $node)
+    {
+        $links = [];
+
+        $links[] = [
+            'rel' => 'self',
+            'href' => $this->generator->generate(
+                'api_node_get',
+                ['uuid' => $node['uuid']]
+            )
+        ];
+
+        foreach ($node['relations'] as $relation) {
+            if ($relation['direction'] === Relationship::DirectionOut) {
+                $links[] = [
+                    'rel' => $relation['type'],
+                    'href' => $this->generator->generate(
+                        'api_node_get',
+                        $relation['endNode']
+                    )
+                ];
+            }
+        }
+
+        $node['_links'] = $links;
+
+        return $node;
+    }
+}
