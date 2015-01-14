8afd78a6259e8db048ce5187288ac55bd4eae5ee
Baptiste Langlade <baptouuuu@gmail.com>
2015-01-14 20:39:01 +0100

    use apache user to run symfony commands

--- docker/init.sh
+++ docker/init.sh
@@ -6,8 +6,8 @@ while [[ RET -ne 0 ]]; do
     mysql -h mysql -uroot -proot -e 'exit' > /dev/null 2>&1; RET=$?
 done
 
-/var/www/app/console doctrine:database:create
-/var/www/app/console doctrine:schema:update --force
-/var/www/app/console innmind:server:add self localhost worker.crawler
+sudo -u www-data sh -c "DOCKER=true /var/www/app/console doctrine:database:create"
+sudo -u www-data sh -c "DOCKER=true /var/www/app/console doctrine:schema:update --force"
+sudo -u www-data sh -c "DOCKER=true /var/www/app/console innmind:server:add self localhost worker.crawler"
 
 /usr/sbin/apache2ctl -D FOREGROUND
