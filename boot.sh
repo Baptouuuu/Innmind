8d462d4f669023eb0cf3035d2f02d47a48dc4c98
Baptiste Langlade <baptouuuu@gmail.com>
2015-01-14 20:38:32 +0100

    force exposing web port on 8080

--- boot.sh
+++ boot.sh
@@ -2,4 +2,4 @@
 docker run -d --name mysql -e MYSQL_ROOT_PASSWORD=root mysql:5.6.22
 docker run -d --name neo4j -p 7474:7474 tpires/neo4j
 docker run -d --name mq -e RABBITMQ_NODENAME=rabbit rabbitmq:3-management
-docker run -d --name innmind --link mysql:mysql --link neo4j:neo4j --link mq:mq -P -v $PWD:/var/www -e DOCKER=true innmind ./docker/init.sh
+docker run -d --name innmind --link mysql:mysql --link neo4j:neo4j --link mq:mq -p 8080:80 -v $PWD:/var/www -e DOCKER=true innmind ./docker/init.sh
